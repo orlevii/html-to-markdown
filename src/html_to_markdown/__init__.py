c562a5411fa75a2280a83fdbf48a804fbe6c65ec
Or Levi <orlevi128@gmail.com>
2025-03-21 00:10:24 +0200

    rename

---
+++ src/html_to_markdown/__init__.py
@@ -0,0 +1,3 @@
+from ._wrapper import html_to_markdown
+
+__all__ = ["html_to_markdown"]
---
+++ tests/test_html_to_markdown.py
@@ -0,0 +1,6 @@
+from html_to_markdown import html_to_markdown
+
+
+def test_binding() -> None:
+    result = html_to_markdown("<h1>Test</h1>")
+    assert result.strip() == "# Test"
